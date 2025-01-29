023ccca9a2e39c14aa29afca570b990e8bdd2a7f
Jacob Millet <jakub.jagla01@gmail.com>
2025-01-29 19:13:20 +0100

    nuke repo :)

--- data/scripts/get_files.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-pip install cityscapesscripts
-
-mkdir -p archives
-csDownload -d archives gtFine_trainvaltest.zip camera_trainvaltest.zip leftImg8bit_trainvaltest.zip
-
-unzip -n archives/gtFine_trainvaltest.zip -d ./
-unzip -n archives/camera_trainvaltest.zip -d ./ 
-unzip -n archives/leftImg8bit_trainvaltest.zip -d ./
